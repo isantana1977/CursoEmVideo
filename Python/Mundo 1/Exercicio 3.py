512aa0636c49814af9a2e040bd41c4e83a3f585a
isantana1977 <ivansantana.sax@gmail.com>
2021-07-15 17:43:13 -0300

    Atualização HTML e CSS index.html

---
+++ Python/Mundo 1/Exercicio 3.py
@@ -0,0 +1,5 @@
+#Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+num1 = input("Primeiro Número: ")
+num2 = input("Segundo Número: ")
+soma = num1 + num2
+print('A soma entre ', num1,'e ', num2, 'é ', soma)
---
+++ Python/Mundo 1/ex002.py
@@ -0,0 +1,8 @@
+#Crie um script Python que leia o dia, o mês e o ano de nascimento de uma  pessoa e mostre uma mensagem com a data
+#formatada
+
+dia = input("Qual o dia de seu nascimento? ")
+mes = input("Em que mês você nasceu? ")
+ano = input("Qual o ano do seu nascimento? ")
+
+print('Você nasceu no dia', dia, 'de', mes,'de', ano, ', correto?')
